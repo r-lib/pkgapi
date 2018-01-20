b0afd3a9f30f642d512f371a0efe00dceeb5e0a7
Kirill Müller <krlmlr+r@mailbox.org>
2018-01-20 10:08:11 +0100

    need with_dir() now

--- R/roclet.R
+++ R/roclet.R
@@ -37,7 +37,7 @@ roclet_output.roclet_api <- function(x, results, base_path, ...) {
   # FIXME: write_if_different()
   writeLines(results, API)
 
-  usethis::use_build_ignore(file_name, base_path = base_path)
+  withr::with_dir(base_path, usethis::use_build_ignore(file_name))
 
   API
 }
